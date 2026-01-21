04d3ebee77278f3290a6f8a7790f76cd3c5a690f
Mahi-agrawal84 <agrawalmahi18012006@gmail.com>
2026-01-21 19:43:36 +0530

    .

--- BasicJavascript/the main app.js
+++
@@ -1,7 +0,0 @@
-// app.js
-const { logActivity } = require("./logger");
-
-// Simulate user flow
-logActivity("User 'Amit' logged in.");
-logActivity("User searched for 'Nodejs books'.");
-logActivity("User logged out.");
